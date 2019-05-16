dba433e93d804f622b69d312592e078bbfae4a65
Tim Cramer <tstave217@gmail.com>
2019-05-16 13:14:48 -0500

    Updating Config file
    I am only using 1 environment right now, simplifying config and adding prefix value

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+const {prefix, token} = require('./src/main/resources/config');
 // require the discord.js module
 const Discord = require('discord.js');
 
 // create a new Discord Client
 const client = new Discord.Client();
 
-const config_all = require('./src/main/resources/config.json');
-
-// just hard-code dev env for now
-// TODO : Make this dynamic based on actual env
-const env = "development";
-
-let config = config_all[env];
 // Once the client is 'ready', run this code
 // This event will only trigger one time after logging in
 client.once('ready', () => {
@@ -18,5 +12,12 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+client.on('message', message => {
+    if(message.content === (`${prefix}server`)) {
+        // reply to ping
+        message.channel.send(`This server's name is: ${message.guild.name}`);
+    }
+});
+
 // log into Discord with your app's token
-client.login(config.apiKey);
+client.login(token);
