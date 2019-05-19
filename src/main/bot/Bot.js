1c6d7b57a3f6631c1bf4a0ad813777da5496c971
Tim Cramer <tstave217@gmail.com>
2019-05-19 14:23:28 -0500

    Adding a response bot
    Adding a simple response bot that can handle single words and respond to them if the message entered is within a certain length (dont want to parse someones copy-pasta novel). Also having the response bot stop at the first message it sees, as I dont want the bot to spam with responses if multiple words are mentioned. And to exit

--- src/main/bot/Bot.js
+++ src/main/bot/Bot.js
@@ -1,3 +1,5 @@
+const responseBotInitiators = require("./utils/responseBotInitiators");
+
 const {defaultCooldown, prefix} = require('../resources/config');
 
 const Discord = require("discord.js");
@@ -33,32 +35,16 @@ class Bot extends Discord.Client {
 
     setInitialValuesFromMessage(message) {
         this.message = message;
-        let args = message.content.slice(prefix.length).split(/ +/);
-        let commandName = args.shift().toLowerCase();
 
-        console.log('args', args);
+        this.updateCommands();
+
+        let args = message.content.slice(prefix.length).split(/ +/);
+        this.args = args;
+
+        let commandName = args.shift().toLowerCase();
 
         this.activeCommandName = commandName;
         this.activeCommand = this.commands.get(commandName) || this.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-        this.args = args;
-
-        this.updateCommands();
-    }
-
-    /**
-     *
-     * @returns {Array}
-     */
-    getArgs() {
-        return this.args;
-    }
-
-    /**
-     *
-     * @returns {string}
-     */
-    getActiveCommandName() {
-        return this.activeCommandName;
     }
 
     /**
@@ -82,15 +68,14 @@ class Bot extends Discord.Client {
 
     // get the current cooldowns
     updateCooldowns() {
-        let activeCommand = this.getActiveCommand();
         // if the command does not exist in the cooldowns collection, add it
-        if (!this.cooldowns.has(activeCommand.name)) {
-            this.cooldowns.set(activeCommand.name, new Discord.Collection());
+        if (!this.cooldowns.has(this.activeCommand.name)) {
+            this.cooldowns.set(this.activeCommand.name, new Discord.Collection());
         }
 
         const now = Date.now();
-        const timestamps = this.cooldowns.get(activeCommand.name);
-        const cooldownAmt = (activeCommand.cooldown || defaultCooldown) * 1000;
+        const timestamps = this.cooldowns.get(this.activeCommand.name);
+        const cooldownAmt = (this.activeCommand.cooldown || defaultCooldown) * 1000;
         // if the author already has a timestamp created
         if (timestamps.has(this.message.author.id)) {
             const expiry = timestamps.get(this.message.author.id) + cooldownAmt;
@@ -98,7 +83,7 @@ class Bot extends Discord.Client {
             // exit and send message with how much longer to wait
             if (now < expiry) {
                 const timeLeft = (expiry - now) / 1000;
-                return this.message.reply(`please wait ${timeLeft.toFixed(1)} more seconds to use the ${activeCommand.name} command.`)
+                return this.message.reply(`please wait ${timeLeft.toFixed(1)} more seconds to use the ${this.activeCommand.name} command.`)
             }
         }
 
@@ -108,32 +93,73 @@ class Bot extends Discord.Client {
         setTimeout(() => timestamps.delete(this.message.author.id), cooldownAmt);
     }
 
+    /**
+     * Checks to see if the incoming message contains anything we want to respond to outside of a command
+     * @returns {Message}
+     */
+    respondToResponseMessageAndEnd() {
+        // we do not want to send multiple responses
+        // if this is a command being executed by the user
+        if (this.isCommandMessage() || this.isBotMessage()) return null;
+
+        let messageAsArray = this.message.content.split(/ +/);
+        if(messageAsArray.length > 0 && messageAsArray.length < 30) {
+            messageAsArray.map(word => {
+                if(responseBotInitiators[word] !== undefined) {
+                    return this.message.reply(responseBotInitiators[word].responses[0].imgUrl ? responseBotInitiators[word].responses[0].imgUrl : responseBotInitiators[word].responses[0].message);
+                }
+            });
+            // message was just a normal message
+        }
+        return null;
+    }
+
+    /**
+     * Checks to see if the incoming message is a valid command for the bot
+     * @param message
+     */
+    isCommandMessage() {
+        return this.message.content.startsWith(prefix);
+    }
+
+    /**
+     * Check to see if the incoming message is originating from a bot
+     * @returns {boolean}
+     */
+    isBotMessage(message) {
+        return message ? message.author.bot : this.message.author.bot;
+    }
+
     engage() {
 
         // Once the client is 'ready', run this code
         // This event will only trigger one time after logging in
         this.once('ready', () => {
-            // console.log(`Logged in as ${client.user.tag}!`);
             console.log('Ready!');
         });
 
         this.on('message', message => {
+            // I dont want to process any bot messages being added to the message queue at this time
+            if(this.isBotMessage(message)) return;
+
             this.setInitialValuesFromMessage(message);
 
-            // Message does not start with prefix or the message author is a bot
-            if (!message.content.startsWith(prefix) || message.author.bot) return;
+            this.respondToResponseMessageAndEnd();
 
-            // if there is no command, exit
+            // At this point, if the message is not a command, we want to exit
+            if (!this.isCommandMessage()) return;
+
+            // if there is no command, exit... just double checking that the command is a valid one
             if (!this.getActiveCommand()) return;
 
             // if the command is guildOnly or the channel type is not text, exit
             if (this.getActiveCommand().guildOnly && message.channel.type !== 'text') {
                 return message.reply('I can\'t execute that command here...');
             }
-
             // if the command has arguments, and the lenght of the args is not empty or 0
-            if (this.getActiveCommand().args && !args.length) {
+            if (this.getActiveCommand().args && !this.args.length) {
                 let reply = 'Please provide the proper arguments for your command.';
+
                 if(this.getActiveCommand().usage) {
                     reply += `\nThe proper usage is: \`${prefix}${this.getActiveCommand().name} ${this.getActiveCommand().usage}\``;
                 }
@@ -141,13 +167,15 @@ class Bot extends Discord.Client {
                 return message.reply(reply);
             }
 
-            this.updateCooldowns()
+            this.updateCooldowns();
 
             try {
-                this.getActiveCommand().execute(message, this.getArgs());
+                this.getActiveCommand().execute(message, this.args);
             } catch (error) {
                 console.error(error);
-                message.reply(`There was an error attempting to execute command: ${activeCommand}`);
+                message.reply(`There was an error attempting to execute command: ${this.activeCommand}`);
+            } finally {
+                // todo: empty golbal vars
             }
 
 
---
+++ src/main/bot/utils/responseBotInitiators.js
@@ -0,0 +1,26 @@
+module.exports = responseBotInitiators =
+    {
+        'bling': {
+            'responses': [
+                {
+                    'name': 'bling-response-1',
+                    'imgUrl': null,
+                    'message': 'Shiny! You said "bling".'
+                },
+            ]
+        },
+        'beans': {
+            'responses': [
+                {
+                    'name': 'beans-response-1',
+                    'imgUrl': 'https://bit.ly/2HIYy0K',
+                    'message': null
+                },
+                {
+                    'name': 'beans-response-2',
+                    'imgUrl': null,
+                    'message': 'I\;m thinking about thos Beans'
+                },
+            ]
+        }
+    };
