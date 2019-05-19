1b40d22357fe646dcd331e9cd65375be67aa5009
Tim Cramer <tstave217@gmail.com>
2019-05-19 14:23:48 -0500

    Logging cleanup

--- src/main/bot/commands/help.js
+++ src/main/bot/commands/help.js
@@ -6,11 +6,11 @@ module.exports = {
     aliases: ['commands'],
     usage: '[command name]',
     cooldown: 5,
+    isEnabled: true,
     execute(message, args) {
         const data = [];
         const { commands } = message.client;
 
-        console.log('args: {}', args);
         if (!args.length) {
             data.push('Here\'s a list of all our commands:');
             data.push(commands.map(command => command.name).join(', '));
